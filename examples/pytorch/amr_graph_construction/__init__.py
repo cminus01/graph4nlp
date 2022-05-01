2e0e700ad4b0bac65d7a6294c1ff187edf212e4d
cminus <28683036+cminusQAQ@users.noreply.github.com>
2022-05-01 18:45:14 +0800

    fix the embedding construction

--- examples/pytorch/amr_graph_construction/__init__.py
+++ examples/pytorch/amr_graph_construction/__init__.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 from .amr_graph_construction import AMRGraphConstruction
 
 __all__ = [
     "AMRGraphConstruction",
-=======
-from .amr_graph_construction import AmrGraphConstruction
-
-__all__ = [
-    "AmrGraphConstruction",
->>>>>>> fix some bug in graph embedding
 ]
